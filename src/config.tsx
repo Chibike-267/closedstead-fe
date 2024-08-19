31dc1fc6a31bf0ed565071c199764d1add186700
Chibike-267 <paulinuschukwubuikem98@gmail.com>
2024-08-19 10:18:04 +0100

    project

---
+++ src/config.tsx
@@ -0,0 +1,6 @@
+//while working on the project,
+//configure your own end point if on local
+//you can also store all your variables here in this file, stay jiggy!
+export const API_ENDPOINT = "http://localhost:3000";
+export const GOOGLE_CLIENT_ID = "285430883101-lcsplktg26bp10g0211746c1si57lifa.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET = "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";
